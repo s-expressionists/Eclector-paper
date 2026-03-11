fae900c86e47af0272e8c97a030d21298a10e965
Jan Moringen <jan.moringen@posteo.de>
2026-03-11 12:18:01 +0100

    Add reading with CST result to benchmark scripts

--- benchmark/run.lisp
+++ benchmark/run.lisp
@@ -2,14 +2,18 @@
   '(("flexi-streams" . #P"enc-cn-tbl.lisp")
     ("screamer"      . #P"screamer.lisp")))
 
-(ql:quickload (list* "alexandria" "eclector" "the-cost-of-nothing"
-                     (mapcar #'first *cases*)))
+(ql:quickload (list* "alexandria"
+                     "eclector"
+                     "eclector-concrete-syntax-tree"
+                     "the-cost-of-nothing"
+                     (mapcar #'first *cases*))
+              :silent t)
 
 (defun call-with-open-stream (system file continuation)
   (let* ((directory (ql:where-is-system system))
          (filename  (merge-pathnames file directory))
          (content   (alexandria:read-file-into-string filename)))
-    (format *trace-output* "  ~:D lines~%" (count #\Newline content))
+    (format *trace-output* " ~7:D lines" (count #\Newline content))
     #+sbcl (sb-ext:gc :full t)
     #+ccl  (gc)
     (the-cost-of-nothing:benchmark
@@ -18,9 +22,9 @@
 
 (defun test-cases (continuation)
   (loop :for (system . file) :in *cases*
-        :do (format *trace-output* "  ~A - ~A~%" system file)
+        :do (format *trace-output* "  ~14A ~16A" system file)
             (let ((time (call-with-open-stream system file continuation)))
-              (format *trace-output* "    ~D s~%" time))))
+              (format *trace-output* "    ~,3F s~%" time))))
 
 (defun read-with-intrinsic-reader ()
   (format *trace-output* "~A ~A builtin reader~%"
@@ -38,6 +42,17 @@
      (loop :for form = (eclector.reader:read stream nil stream)
            :until (eq form stream)))))
 
+(defun read-with-eclector/cst ()
+  (format *trace-output* "~A ~A Eclector + CST~%"
+          (lisp-implementation-type) (lisp-implementation-version))
+  (test-cases
+   (lambda (stream)
+     (loop :for form = (eclector.concrete-syntax-tree:read
+                        stream nil stream)
+           :until (eq form stream)))))
+
 (read-with-intrinsic-reader)
 
 (read-with-eclector)
+
+(read-with-eclector/cst)
